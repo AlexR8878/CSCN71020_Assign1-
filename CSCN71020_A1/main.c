229ed0c2be290e240d7afc03df46b1cad8b06cb3
Alex Ratchev <alexratchev3@gmail.com>
2024-12-13 21:48:41 -0500

    Added multiplcation...

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -1,61 +1,84 @@
 #include <stdio.h>
 
+// Function declarations
 void printWelcomeMenu();
 void printOptions();
 void add();
 void sub();
+void multiply();
 
-int main (void) {
-	printWelcomeMenu();
+// Main function
+int main(void) {
+    printWelcomeMenu();
+    printOptions();
 
-	printOptions();
+    int inputNum;
 
-	int inputNum;
+    printf("Enter operation number: ");
+    scanf_s("%d", &inputNum);  // Use standard scanf
 
-	printf("Enter operation number: ");
-	scanf_s("%d", &inputNum);
+    switch (inputNum) {
+        case 1:
+            add();
+            break;
+        case 2:
+            sub();
+            break;
+        case 3:
+            multiply();
+            break;
+        default:
+            printf("Invalid operation number.\n");
+            break;
+    }
 
-	switch (inputNum)
-	{
-	case 1:
-		add();
-		break;
-	case 2:
-		sub();
-		break;
-	}
-
+    return 0;
 }
 
+// Function to print the welcome menu
 void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   BCS Calculator   **\n");
-	printf(" **********************\n");
+    printf(" **********************\n");
+    printf("**   Welcome to the   **\n");
+    printf("**   BCS Calculator   **\n");
+    printf(" **********************\n");
 }
 
-	void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
+// Function to print options
+void printOptions() {
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3. Multiply\n");
 }
-	void add() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
-	return 0;
-	}
 
+// Function to add two numbers
+void add() {
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 + num2;
+    printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+// Function to subtract two numbers
 void sub() {
-	double num1, num2, result2;
-	printf("Enter the first value: ");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value: ");
-	scanf_s("%lf", &num2);
-	result2 = num1 - num2;
-	printf("%lf - %lf = %lf\n", num1, num2, result2);
-	return 0;
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 - num2;
+    printf("%lf - %lf = %lf\n", num1, num2, result);
+}
+
+// Function to multiply two numbers
+void multiply() {
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 * num2;
+    printf("%lf * %lf = %lf\n", num1, num2, result);
 }
